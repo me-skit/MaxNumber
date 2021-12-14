fcbcc5647d643ee6a584db51d828e3f29eb8ef61
Meme <meme_es@hotmail.com>
2021-12-14 01:37:14 -0600

    Agrega codigo para los puntos de bonos
    Este commit agrega código para una aplicación de consola en el que el
    usuario debe ingresar 10 números diferentes separados por espacios de
    donde la aplicacion muestra cual es e mayor de los números.

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] subs = { "1" };
+
+            List<string> numbers = new List<string>();
+
+            while ( numbers.Count != 10)
+            {
+                Console.WriteLine("Ingrese 10 numeros diferentes entre sí, separados por espacios: ");
+                var strNumbers = Console.ReadLine();
+                numbers = strNumbers.Split(' ').Distinct().ToList();
+            }
+
+            int max = Int32.MinValue;
+            bool success = true;
+            foreach (var item in numbers)
+            {
+                var currentNumber = 0;
+                //var currentNumber = Int32.Parse(item);
+                success = int.TryParse(item, out currentNumber);
+                if (success)
+                {
+                    if (currentNumber > max)
+                    {
+                        max = currentNumber;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Valor no numerico ingresado");
+                    break;
+                }
+            }
+
+            Console.WriteLine("El número mayor es: {0}", max);
+            Console.Write("Ejecución finalizada, precione cualquier tecla");
+            Console.ReadKey();
+        }
+    }
+}
